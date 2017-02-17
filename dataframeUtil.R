755638d7464377a58b461a3863682246d3ebeb43
Christian Medeiros Adriano <christian.adriano@gmail.com>
2017-02-17 18:20:16 +0100

    Function names, utility functions, adn updated comments

--- dataframeUtil.R
+++ dataframeUtil.R
@@ -1,4 +1,4 @@
-## Utility functions
+## Remove lines with duplicated entries in a file
 
 #Replace values
 replaceValues <- function (dataframe,currentValue,newValue){
@@ -23,10 +23,14 @@ removeLinesAboveValue <- function (dataframe, columNumber, value){
 }
 
 #Remove lines if we now the name of the column
-dataframe<-dataframe[!(dataframe$columnName==value) ,]
+remLinesDataFrame <- function(dataframe,dataframeColumn,value){
+  #dataframe<-dataframe[!(dataframe$columnName==value) ,]
+  dataframe<-dataframe[!(dataframeColumn==value) ,];
+  return(dataframe);
+}
 
 #Remove lines for which the difference between two columns is above the 
-removeLinesColDiffSmallerThanValue <- function (dataframe, columNumber1,columnNumber2,){
+removeLinesColDiffSmallerThanValue <- function (dataframe, columNumber1,columnNumber2,value){
   diff = as.vector(dataframe[,columNumber1]) - as.vector(dataframe[,columnNumber2]);
   lines <- which(diff<value)
   dataframe<- dataframe[-lines,]
@@ -43,5 +47,46 @@ colnames (df) <- names
 df$worker.yoe <- lapply(df$worker.yoe,function(x){as.numeric(as.character(x))})
 df$age <- lapply(df$age,function(x){as.numeric(as.character(x))})
 
+#Utility functions
+#################################################################################                                                                
+
+# Function counts the number of words in a column of the data frame.
+#Words are provided in a vector.
+countWordsInDataFrameColumn<- function(wordVector,dataframe,columNumber){
+  resultVector <-is.na(c(1:length(dataframe[,columNumber])))
+  for (word in wordVector) {
+    count <- countWords(word,dataframe,columNumber)
+    resultVector <- resultVector + count;
+  }
+  return(resultVector);
+}
+
+# Function counts the number of words in a vector 
+#Words are provided in a vector.
+countWordsInVector<- function(wordVector,vector){
+  resultVector <-is.na(c(1:length(vector)))
+  for (word in wordVector) {
+    count <- countWords(word,vector);
+    resultVector <- resultVector + count;
+  }
+  return(resultVector);
+}
+
+# Function counts the number of words in a column of the data frame
+countWords <- function(word, vector){
+  booleanVector <- grepl(word, vector);
+  return(booleanVector*1);
+}
+
+# Function counts the proportion of numbers in a vector
+countNumbers<- function(vector){
+  tab <- table(vector);
+  df <- as.data.frame(tab);
+  total<-sum(df$Freq);
+  percent<-(df$Freq/total)*100
+  return (cbind(df,percent));
+}
+
+##################################################################################
 
 
